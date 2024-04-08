c9ae41c05380592434452af335292420cff34041
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-08 12:05:20 +0300

    Feat: update Todo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,23 @@
 import React, {useState} from 'react'
 
-const TodoForm = () => {
-    
+const TodoForm = ({addTodo}) => {
+    // Empty string
+    const [value, setValue] = useState("")
+
+    // fun that captures  the value of the state when we submit the form
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        addTodo(value)
+
+        setValue("")
+    }
+
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='What is the task today?'/>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='What is the task today?'
+        onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='todo-btn'>
             Add Task
         </button>
